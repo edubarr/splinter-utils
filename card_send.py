d60b788491c6223b7672c216fa5e25f29d1df20c
Eduardo Barros <eduardo.araujo@outlook.com.br>
2021-12-15 21:03:06 -0300

    update selenium and peakmonsters selector

---
+++ card_send.py
@@ -0,0 +1,196 @@
+"""
+BOT em Python para envio de cartas em lote via PeakMonsters
+"""
+import os
+import time
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from tqdm import tqdm
+
+print("\nSCIDU - BOT para envio de cartas em lote via PeakMonsters\n")
+
+# Carrega as contas do arquivo .env
+
+# TODO: AS VEZES O HIVESIGNER NAO VAI COM 1 CLIQUE. CORRIGIR NOME DAS CARTAS. VERIFICAR TROCAR PARA FIND_ELEMENT()
+
+
+def loadAccounts():
+    load_dotenv()
+    userlst = os.getenv("ACCUSERNAME").split(",")
+    postinglst = os.getenv("POSTING_KEY").split(",")
+    activelst = os.getenv("ACTIVE_KEY").split(",")
+    pwlst = list(zip(postinglst, activelst))
+    print(str(len(userlst)) + " accounts loaded!\n")
+    print(userlst)
+    return dict(zip(userlst, pwlst))
+
+# Inicia o Browser
+
+
+def startBrowser():
+    chrome_service = Service("chromedriver.exe")
+    options = Options()
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    options.add_argument("start-maximized")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    return webdriver.Chrome(options=options, service=chrome_service)
+
+
+def main():
+    acc_dict = loadAccounts()
+
+    # Iniciar Browser como driver
+
+    # Definir espera padrão
+
+    # Percorre o dicionário
+    for user, pw_lst in acc_dict.items():
+        driver = startBrowser()
+        wait = WebDriverWait(driver, 30)
+        print(user)
+        driver.get("https://peakmonsters.com/")
+
+        wait.until(EC.element_to_be_clickable(
+            (By.XPATH, '//*[@id="app"]/div[5]/div/div[3]/div/button')))
+        driver.find_element(
+            By.XPATH, '//*[@id="app"]/div[5]/div/div[3]/div/button').click()
+
+        wait.until(EC.element_to_be_clickable(
+            (By.XPATH, "//a[normalize-space()='Login']")))
+        driver.find_element(By.XPATH, "//a[normalize-space()='Login']").click()
+
+        wait.until(EC.element_to_be_clickable(
+            (By.XPATH, "//input[@placeholder='Email / Username']")))
+        elm_user = driver.find_element(By.XPATH,
+                                       "//input[@placeholder='Email / Username']")
+        elm_user.clear()
+        elm_user.send_keys(user)
+
+        elm_pw = driver.find_element(By.XPATH,
+                                     "//input[@placeholder='Password / Posting Key']")
+        elm_pw.clear()
+        elm_pw.send_keys(pw_lst[0])
+
+        driver.find_element(By.XPATH, "//img[@alt='Login']").click()
+
+        wait.until(EC.element_to_be_clickable(
+            (By.XPATH, "//a[normalize-space()='My Cards']")))
+        driver.find_element(By.XPATH,
+                            "//a[normalize-space()='My Cards']").click()
+
+        # wait.until(EC.visibility_of_element_located(
+        #     (By.XPATH, "//li[37]/a/div[1]/input")))
+        driver.find_element(By.XPATH,
+                            "//li[38]/a/div[1]/input").click()
+
+        time.sleep(2)
+
+        cards_rows = len(driver.find_elements(By.XPATH, "//tbody/tr"))
+
+        if cards_rows == 0:
+            print("Account doens't seem to have any cards owned!")
+            driver.close()
+            
+            print("Waiting 3 minutes to avoid Peakmonsters Ban!")
+            time.sleep(200)
+            continue
+
+        for i in range(cards_rows):
+            card = driver.find_element(By.XPATH,
+                                       "//tbody/tr[" + str(i + 1) + "]/td[2]/div/div")
+
+            card_name = card.text.split()[:2]
+
+            card_frame = driver.find_element(By.XPATH,
+                                             "//tbody/tr[" + str(i + 1) + "]/td[2]/div/a/div")
+
+            classes = card_frame.get_attribute('class')
+
+            if "monster-img-gold" in classes:
+                print(card_name)
+                print("Gold")
+            else:
+                print(card_name)
+
+        wait.until(EC.element_to_be_clickable(
+            (By.XPATH, "//table/thead/tr/th[1]/a/i")))
+        # Talvez remover pro headless
+        driver.execute_script("window.scrollTo(0, 0)")
+        driver.find_element(By.XPATH,
+                            "//table/thead/tr/th[1]/a/i").click()
+
+        driver.find_element(By.XPATH, "//i[@class='icon-stack3']").click()
+
+        wait.until(EC.element_to_be_clickable(
+            (By.XPATH, "//button[normalize-space()='Transfer']")))
+        driver.find_element(By.XPATH,
+                            "//button[normalize-space()='Transfer']").click()
+
+        wait.until(EC.element_to_be_clickable(
+            (By.XPATH, "//input[@placeholder='Account Name']")))
+        elm_main_acc = driver.find_element(By.XPATH,
+                                           "//input[@placeholder='Account Name']")
+        elm_main_acc.clear()
+        elm_main_acc.send_keys(os.getenv("MAIN_ACC"))
+
+        wait.until(EC.element_to_be_clickable(
+            (By.XPATH, "//button[@class='btn btn-success btn-xs']")))
+        driver.find_element(By.XPATH,
+                            "//button[@class='btn btn-success btn-xs']").click()
+        wait.until(EC.element_to_be_clickable(
+            (By.XPATH, "//div[@class='sweet-modal theme-light has-title has-content is-visible']//button[@class='btn btn-primary btn-xs'][normalize-space()='Confirm']")))
+        driver.find_element(By.XPATH,
+                            "//div[@class='sweet-modal theme-light has-title has-content is-visible']//button[@class='btn btn-primary btn-xs'][normalize-space()='Confirm']").click()
+
+        time.sleep(3)
+        wait.until(EC.element_to_be_clickable(
+            (By.XPATH, "//a[normalize-space()='Continue']")))
+        # Talvez remover pro headless
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+        driver.find_element(By.XPATH,
+                            "//a[normalize-space()='Continue']").click()
+
+        wait.until(EC.element_to_be_clickable(
+            (By.XPATH, "//input[@id='username']")))
+        elm_hive_login = driver.find_element(By.XPATH,
+                                             "//input[@id='username']")
+        elm_hive_login.clear()
+        elm_hive_login.send_keys(user)
+
+        elm_hive_pw = driver.find_element(By.XPATH, "//input[@id='password']")
+        elm_hive_pw.clear()
+        elm_hive_pw.send_keys(pw_lst[1])
+
+        driver.find_element(By.XPATH,
+                            "//span[@class='checkbox mr-2 -checked']").click()
+
+        driver.find_element(By.XPATH,
+                            "//button[normalize-space()='Login']").click()
+
+        time.sleep(3)
+        wait.until(EC.element_to_be_clickable(
+            (By.XPATH, "//button[normalize-space()='Approve']")))
+        # Talvez remover pro headless
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+        driver.find_element(By.XPATH,
+                            "//button[normalize-space()='Approve']").click()
+
+        print("Waiting 3 minutes to avoid Peakmonsters Ban!")
+
+        time.sleep(10)
+
+        driver.close()
+
+        time.sleep(200)
+
+
+if __name__ == "__main__":
+    main()
